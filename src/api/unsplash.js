58a7c28435b349e299dfd22f4a72cec92d4ed673
ramiz_khan <ramizuddin.khan@rivigo.com>
2019-08-04 01:42:46 +0530

    removed the authorization key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
 	baseURL: 'https://api.unsplash.com',
 	headers: {
 				Authorization:
-				'Client-ID 7ec34d16f48c52c61e373784bc96c1b4c2f38beec7db507b4064495a946f9265'
+				'Client-ID <Your API Authorization key here>'
 			}
 });
